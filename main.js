066685a480f1ee58753465814e457d6210008acc
NANTEGEBABRAH <112953220+NANTEGEBABRAH@users.noreply.github.com>
2022-12-07 10:48:34 +0300

    update

---
+++ main.js
@@ -0,0 +1,17 @@
+// change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', wondow.scrollY > 0)
+})
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click',() => {
+        faq.classList.toggle('open');
+
+    })    
+})
